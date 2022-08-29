4d667fa95d530d1266023b82a114a6361983b70f
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2022-08-29 16:25:56 -0300

    Criando contextos de transações

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+interface transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: transaction[];
+}
+
+interface TransactionProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType);
+
+export function TransactionProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
